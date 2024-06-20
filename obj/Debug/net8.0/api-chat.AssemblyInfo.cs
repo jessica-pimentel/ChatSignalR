b412a0c1a0459161bcbd965ea8c5f1bcbd709012
jessica-pimentel <jppimentel96@hotmail.com>
2024-06-20 19:05:19 -0300

    criando gitignore

--- obj/Debug/net8.0/api-chat.AssemblyInfo.cs
+++ obj/Debug/net8.0/api-chat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-chat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20948566cb4b99daf78c5856f34f72f0de8293b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-chat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-chat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
